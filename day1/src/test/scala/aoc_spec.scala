9dff3283abd388e168d7b4ec1d4d89f6a97b6f7c
John Murray <me@johnmurray.io>
2018-06-22 20:34:39 -0600

    day 1 solution

--- day1/src/test/scala/aoc_spec.scala
+++ day1/src/test/scala/aoc_spec.scala
@@ -2,12 +2,29 @@ import org.specs2.mutable.Specification
 
 class Day1 extends Specification {
 
-  "placeholder" >> {
-    "should be true" >> {
-      aoc.one must_== 1
+  "aoc tests" >> {
+    "test case 1" >> {
+      aoc.solveCaptcha("11") must_== 2
     }
-    "should also be free" >> {
-      aoc.two must_== 2
+
+    "test case 2" >> {
+      aoc.solveCaptcha("1") must_== 0
+    }
+
+    "example cases 1" >> {
+      aoc.solveCaptcha("1122") must_== 3
+    }
+    "example cases 2" >> {
+      aoc.solveCaptcha("1111") must_== 4
+    }
+    "example cases 3" >> {
+      aoc.solveCaptcha("1234") must_== 0
+    }
+    "example cases 4" >> {
+      aoc.solveCaptcha("91212129") must_== 9
+    }
+    "example cases 5" >> {
+      aoc.solveCaptcha("9121442129") must_== 13
     }
   }
 
